f96544af9a87b635750b6e1fce12bc9a9ed455f7
AlphaGuo <74103077+AlphaGuo171@users.noreply.github.com>
2024-04-07 09:16:34 +0800

    主程序

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+import pandas as pd
+import akshare as ak
+import plotly.graph_objs as go
+from plotly.offline import plot
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        stock_code = request.form.get('stock_code')
+        try:
+            data = ak.stock_cyq_em(symbol=stock_code, adjust="")
+            price = ak.stock_zh_a_spot_em()
+            cost_price = price.loc[price['代码']==stock_code]['最新价'].item()
+            
+            df = pd.DataFrame(data)
+            df['日期'] = pd.to_datetime(df['日期'])
+            
+            fig = go.Figure()
+            fig.add_trace(go.Scatter(x=df['日期'], y=df['平均成本'], mode='lines', name='平均成本'))
+            fig.add_trace(go.Scatter(x=df['日期'], y=df['90成本-低'], mode='lines', name='90成本-低'))
+            fig.add_trace(go.Scatter(x=df['日期'], y=df['90成本-高'], mode='lines', name='90成本-高'))
+            fig.add_trace(go.Scatter(x=df['日期'], y=df['70成本-低'], mode='lines', name='70成本-低'))
+            fig.add_trace(go.Scatter(x=df['日期'], y=df['70成本-高'], mode='lines', name='70成本-高'))
+            
+            cost_line = go.Scatter(x=df['日期'], y=[cost_price] * len(df['日期']), mode='lines', line=dict(color='red', dash='dash'))
+            fig.add_trace(cost_line)
+            
+            fig.update_layout(title=stock_code + '成本数据与持股成本对比', xaxis_title='日期', yaxis_title='成本')
+            fig.update_layout(legend=dict(orientation='h', yanchor='bottom', y=1.02, xanchor='right', x=1))
+            
+            plot_div = plot(fig, output_type='div', include_plotlyjs=True)
+            return render_template('index.html', plot_div=plot_div, stock_code=stock_code)
+        except Exception as e:
+            return render_template('index.html', error=str(e))
+    
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
